120d91447a7faeda8a5f235578842f25586149e5
Ime <kirjalikud@gmail.com>
2023-04-18 00:00:12 +0300

    auth from form

--- main.js
+++ main.js
@@ -1,54 +1,59 @@
-// function f1(){
-//     //1. create object request
-//     const xhr = new XMLHttpRequest();
+// const form = document.getElementById('logsData');
+//  function retrieveFormValue(event){
+//      event.preventDefault();
 
-//     //2. where to send and parameters
-//     xhr.open('POST', 'https://01.kood.tech/api/auth/signin')
-//     xhr.setRequestHeader('kirjalikud@gmail.com','ime22312')
+//      const email = form.querySelector('[name="login"]'),
+//        password = form.querySelector('[name="password,"]')
 
-//     //3. server response
-//     xhr.onload = function () {
-//         console.log (xhr.status);
-//         console.log (xhr.response);
-        
-//     }
-//     //4. send request
-//     xhr.send();
-// }
-
-// f1(); 
+//     let user = {
+//     email: email.value,
+//     password: password.value 
+//   };
+//   console.log(user)
+//  }
+//  form.addEventListener('submit',retrieveFormValue)
+//  retrieveFormValue();
 
 
-// const formElement = documnt.getElementById('logsData'); // извлекаем элемент формы
-// formElement.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   const formData = new FormData(formElement); // создаём объект FormData, передаём в него элемент формы
-//   // теперь можно извлечь данные
-//   const name = formData.get('login'); 
-//   const surname = formData.get('pass'); 
-//   console.log(name,surname)
+// let formData = {};
+// const form = document.querySelector('form');
+// const LS =localStorage;
+// form.addEventListener('input', function(event){
+//     formData[event.target.name] = event.target.value;
+//     LS.setItem('formData', JSON.stringify(formData));
+//     console.log(LS)
 // });
 
+
+// function login(){
+// let log = document.getElementById('login').value;
+// let pass = document.getElementById('pass').value;
+
+// //console.log(log,pass);
+// }
+// login();
+
 async function signRequest (){
-
-let user = {
-    email: 'kirjalikud@gmail.com',
-    password: 'ime911921'
-  };
- 
-  console.log(user);
- let response = await fetch('https://01.kood.tech/api/auth/signin', {
-    method: 'POST',
-    body: JSON.stringify(user),
-    headers: new Headers( {
-      "Authorization": 'Basic ' + btoa(`${user.email}:${user.password}`)
-    }),
-    
-  });
-let result = await response.json();
-alert(result.message);
-
-}
-signRequest();
+    let user = {
+        email: document.getElementById('login').value,
+        password: document.getElementById('pass').value
+      };
+     
+      console.log(user);
+     let response = await fetch('https://01.kood.tech/api/auth/signin', {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: new Headers( {
+          "Authorization": 'Basic ' + btoa(`${user.email}:${user.password}`)
+        }),
+        
+      });
+      //console.log("log= ",btoa(user.email),"pass= ",btoa(user.pass))
+      //console.log(btoa(`${user.email}:${user.password}`))
+    let result = await response.json();
+    console.log(result);
+    alert(result.message);
+    }
+    //signRequest();
 
 
