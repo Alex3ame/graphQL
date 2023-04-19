511f1bfe568e62dc5f283b2a22133aa3c5b1608c
Ime <kirjalikud@gmail.com>
2023-04-19 16:54:31 +0300

    auth form with token

--- main.js
+++ main.js
@@ -1,59 +1,62 @@
-// const form = document.getElementById('logsData');
-//  function retrieveFormValue(event){
-//      event.preventDefault();
 
-//      const email = form.querySelector('[name="login"]'),
-//        password = form.querySelector('[name="password,"]')
+let LS = localStorage
 
-//     let user = {
-//     email: email.value,
-//     password: password.value 
-//   };
-//   console.log(user)
-//  }
-//  form.addEventListener('submit',retrieveFormValue)
-//  retrieveFormValue();
+function clearLS(){
+  LS.clear();
+  checkLS();
+}
 
-
-// let formData = {};
-// const form = document.querySelector('form');
-// const LS =localStorage;
-// form.addEventListener('input', function(event){
-//     formData[event.target.name] = event.target.value;
-//     LS.setItem('formData', JSON.stringify(formData));
-//     console.log(LS)
-// });
+//create login form
+function checkLS(){
+  console.log(localStorage.length)
+  if (localStorage.length == 0){
+    document.getElementById('myForm').innerHTML= 
+    '<h1>Enter your email and password</h1> <input type="text" name="login" id="login" placeholder="Login"></input> <input type="text" name="pass" id="pass" placeholder="Password"> <span onclick="signRequest()">Send</span>';
+  }else{
+    document.getElementById('myForm').innerHTML='<span onclick="clearLS()">Logout</span>';
+  }
+}
+checkLS();
 
 
-// function login(){
-// let log = document.getElementById('login').value;
-// let pass = document.getElementById('pass').value;
 
-// //console.log(log,pass);
-// }
-// login();
+
 
 async function signRequest (){
+
     let user = {
         email: document.getElementById('login').value,
         password: document.getElementById('pass').value
       };
+    
+      
      
-      console.log(user);
+      //console.log(user);
      let response = await fetch('https://01.kood.tech/api/auth/signin', {
         method: 'POST',
         body: JSON.stringify(user),
-        headers: new Headers( {
+        headers: new Headers({
           "Authorization": 'Basic ' + btoa(`${user.email}:${user.password}`)
         }),
         
       });
-      //console.log("log= ",btoa(user.email),"pass= ",btoa(user.pass))
-      //console.log(btoa(`${user.email}:${user.password}`))
-    let result = await response.json();
-    console.log(result);
-    alert(result.message);
+
+      //clear form
+      const form = document.getElementById("myForm");
+      form.reset();
+
+      //error check
+      if (response.ok != true){
+        alert("Error HTTPS: " + response.status + " User does not exist or password incorrect")
+  
+      }else{
+        let result = await response.json();
+        LS == localStorage.setItem('token',result); 
+        checkLS();
+        //console.log(response.status);
+        //alert(result.message);
+      }
+      
     }
-    //signRequest();
 
 
